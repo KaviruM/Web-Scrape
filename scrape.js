595e32b37d46c176b5e3fb193f1faba9ae9c9c58
Kaviru Mendis <kaviru.mendis@gmail.com>
2025-10-22 09:10:59 +0530

    Download Image Test

--- scrape.js
+++ scrape.js
@@ -1,5 +1,11 @@
 import { JSDOM } from 'jsdom';
 import fs from 'fs';
+import path from 'path';
+
+// Create images folder if it doesn't exist
+if (!fs.existsSync('./images')) {
+  fs.mkdirSync('./images');
+}
 
 async function fetchAnimalDetails(url) {
   const resp = await fetch(url);
@@ -22,7 +28,28 @@ async function fetchAnimalDetails(url) {
   return output;
 }
 
+// Image download function
+async function imagedownload(url, filepath) {
+  const resp = await fetch(url);
+  const buffer = await resp.arrayBuffer();
+  fs.writeFileSync(filepath, Buffer.from(buffer));
+}
+
+// download checkmark png
+const downloadCheckmark = async () => {
+  const url = 'https://a-z-animals.com/wp-content/themes/fwp-az-animals-theme/assets/images/checkmark.png';
+  const logoUrl = 'https://a-z-animals.com/wp-content/themes/fwp-az-animals-theme/assets/images/logo/logo.png';
+  const filepath = path.join('./images', 'checkmark.png');
+  const logoFilepath = path.join('./images', 'logo.png');
+  await imagedownload(url, filepath);
+  await imagedownload(logoUrl, logoFilepath);
+  console.log('✓ Checkmark and Logo downloaded');
+};
+
 async function scrapeAnimals() {
+
+  await downloadCheckmark();
+
   const resp = await fetch('https://a-z-animals.com/animals/');
   const html = await resp.text();
   const dom = new JSDOM(html);
